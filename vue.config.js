8ebae5c3523d59f19411bde12a78a86f31e2935e
michael-franke <the.secret.goldfish@me.com>
2024-11-11 21:38:58 +0100

    fixed config

--- vue.config.js
+++ vue.config.js
@@ -20,18 +20,9 @@ module.exports = {
     stylelint: {}
   },
   publicPath:
-<<<<<<< HEAD
-    process.env.NODE_ENV === 'production' && process.env.REPO_NAME
-      ? '/' +
-        process.env.REPO_NAME +
-        __dirname.substring(process.env.GITHUB_WORKSPACE.length)
-      : '/'
-=======
       process.env.NODE_ENV === 'production' && process.env.REPO_NAME
           ? '/' +
           process.env.REPO_NAME +
           __dirname.substring(process.env.GITHUB_WORKSPACE.length)
           : '/'
->>>>>>> 17ce0ffaa58122b8036adfa81941f66b191db841
 };
-
